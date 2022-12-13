09aadb284137fecac932a0af33936b0de4099a97
gandradep <eventsonido@gmail.com>
2022-12-13 16:45:38 +0100

    Create invoice_items, treatments and joinTable tables

--- schema.sql
+++ schema.sql
@@ -18,3 +18,32 @@ CREATE TABLE invoices (
   generated_at TIMESTAMP NOT NULL,
   payed_at TIMESTAMP NOT NULL,
   medical_history_id INT REFERENCES medical_histories(id));
+
+/* Create invoice_items */
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL (8,2),
+    quantity INT,
+    total_price DECIMAL (8,2),
+    invoice_id INT,
+    treatment_id INT,
+    FOREIGN KEY(invoice_id) REFERENCES invoices (id),
+    FOREIGN KEY(treatment_id) REFERENCES treatments (id),	
+);
+
+/* Create treatments */
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR,
+    name VARCHAR,   	
+);
+
+/*Create join table medical_histories and treatments */
+
+CREATE TABLE medical_treatment (
+    treatment_id INT,
+    med_history_id INT,
+    FOREIGN KEY(treatment_id) REFERENCES treatments (id),
+    FOREIGN KEY(med_history_id) REFERENCES medical_histories (id),   	
+);
